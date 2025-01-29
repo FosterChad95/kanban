270efe5cb0509fda480402534f1cc7be6ca1ed9f
Chad Foster <chad.foster@fgsglobal.com>
2025-01-29 13:13:08 -0500

    feat: add initial style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,21 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-jakarta-sans)", "ui-sans-serif", "system-ui"],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "main-purple": "#635FC7",
+        "main-purple-light": "#A8A4FF",
+        black: "#000112",
+        "very-dark-gray": "#20212C",
+        "dark-gray": "#2B2C37",
+        "lines-dark": "#3E3F4E",
+        "lines-light": "#E4EBFA",
+        "medium-gray": "#828FA3",
+        "light-gray": "#F4F7FD",
+        red: "#EA5555",
+        "red-hover": "#FF9898",
       },
     },
   },
