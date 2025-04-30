1c22bb5e99c74d47edbd5812891b4efb38ac3887
Chad Foster <chad.foster@fgsglobal.com>
2025-04-30 17:58:02 -0400

    feat: story adjustments & add reusable button

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,15 +3,23 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
+    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  staticDirs: ["../public"],
+  docs: {
+    autodocs: "tag",
+  },
 };
 export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,23 @@
+import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: "light",
+      values: [
+        { name: "light", value: "#F4F7FD" },
+        { name: "dark", value: "#20212C" },
+      ],
+    },
+  },
+};
+
+export default preview;
