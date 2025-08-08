878f055c0dfdcb0edeb92af45a18177dabb5292f
Chad Foster <fosterchad95@gmail.com>
2025-08-08 15:01:43 -0400

    feat: finish desktop styles board

--- src/components/content/Board/Board.tsx
+++ src/components/content/Board/Board.tsx
@@ -7,10 +7,10 @@ type BoardProps = {
 
 const Board: React.FC<BoardProps> = ({ columns }) => {
   return (
-    <div className="flex flex-col flex-1 min-h-screen ">
+    <div className="flex flex-col flex-1 min-h-screen bg-light-gray p-6">
       {columns.length === 0 ? (
-        <div className="flex flex-1 flex-col items-center justify-center bg-light-gray rounded-lg shadow">
-          <p className="mb-6 text-gray-500">
+        <div className="flex flex-1 flex-col items-center justify-cente rounded-lg shadow">
+          <p className="mb-6 text-medium-gray font-bold text-lg">
             This board is empty. Create a new column to get started.
           </p>
           <button className="bg-[#635fc7] text-white rounded-full px-6 py-2 font-semibold hover:bg-[#a8a4ff] transition">
@@ -22,7 +22,7 @@ const Board: React.FC<BoardProps> = ({ columns }) => {
           {columns.map((column) => (
             <Column key={column.name} column={column} />
           ))}
-          <button className="min-w-[280px] w-72 flex-shrink-0 flex items-center justify-center bg-gradient-to-b from-[#e9effa] to-[#f4f7fd] rounded-lg border-2 border-dashed border-[#635fc7] text-[#635fc7] font-semibold hover:bg-[#d6e0ff] transition">
+          <button className="min-w-[280px] w-72 flex-shrink-0 flex items-center justify-center bg-gradient-to-b from-[#e9effa] to-[#f4f7fd] rounded-lg border-[#635fc7] text-[#635fc7] font-semibold hover:bg-[#d6e0ff] transition">
             + New Column
           </button>
         </div>
